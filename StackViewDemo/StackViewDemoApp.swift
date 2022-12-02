348ef47df0598363b666318d5557736929743853
Benny Rinaldo <rinaldobdg@yahoo.com>
2022-12-02 15:45:59 +0700

    Initial Commit

---
+++ StackViewDemo/StackViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackViewDemoApp.swift
+//  StackViewDemo
+//
+//  Created by Benny Rinaldo on 02/12/22.
+//
+
+import SwiftUI
+
+@main
+struct StackViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
