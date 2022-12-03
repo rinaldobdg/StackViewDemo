c4f00b411ee61fc510c0512752fc3a7798c08da8
Benny Rinaldo <rinaldobdg@yahoo.com>
2022-12-03 11:18:49 +0700

    first commit

--- StackViewDemo/ContentView.swift
+++ StackViewDemo/ContentView.swift
@@ -8,19 +8,126 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @Environment(\.verticalSizeClass) var verticalSizeClass
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        VStack (spacing: 20) {
+            VStack {
+                Text("Instant Developer")
+                    .fontWeight(.medium)
+                    .font(.system(size: 40))
+                    .foregroundColor(.indigo)
+                
+                Text("Get help from experts in 15 minutes")
+            }
+           
+            HStack (alignment: .bottom, spacing: 10) {
+                Image("user1")
+                    .resizable()
+                    .scaledToFit()
+                
+                Image("user2")
+                    .resizable()
+                    .scaledToFit()
+                
+                Image("user3")
+                    .resizable()
+                    .scaledToFit()
+            }
+            .padding(.horizontal, 20)
+            
+            Text("Need help with coding problems? Register!")
+           
+            Spacer()
+            
+            if verticalSizeClass == .compact {
+                HSignUpButtonGroup()
+            } else {
+                VSignUpButtonGroup()
+            }
         }
-        .padding()
+        .padding(.top, 30)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .previewDevice(PreviewDevice(rawValue: "iPhone 14 Pro Max"))
+                .previewDisplayName("iPhone 14 Pro Max")
+            
+            ContentView()
+                .previewDevice(PreviewDevice(rawValue: "iPhone 14 Pro"))
+                .previewDisplayName("iPhone 14 Pro")
+                .previewInterfaceOrientation(.landscapeLeft)
+            
+            ContentView()
+                .previewDevice(PreviewDevice(rawValue: "iPad Air (5th generation)"))
+                .previewDisplayName("iPad Air")
+            
+            ContentView()
+                .previewDevice(PreviewDevice(rawValue: "iPad Pro (12.9-inch) (6th generation)"))
+                .previewDisplayName("iPad Pro 12.9-inch")
+            
+        }
+    }
+}
+
+struct VSignUpButtonGroup: View {
+    var body: some View {
+        VStack {
+            Button {
+                
+            } label: {
+                Text("Sign Up")
+            }
+            .frame(width: 200)
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.indigo)
+            .cornerRadius(10)
+            
+            Button {
+                
+            } label: {
+                Text("Log In")
+            }
+            .frame(width: 200)
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.gray)
+            .cornerRadius(10)
+        }
+    }
+}
+
+struct HSignUpButtonGroup: View {
+    var body: some View {
+        HStack {
+            Button {
+                
+            } label: {
+                Text("Sign Up")
+            }
+            .frame(width: 200)
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.indigo)
+            .cornerRadius(10)
+            
+            Button {
+                
+            } label: {
+                Text("Log In")
+            }
+            .frame(width: 200)
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.gray)
+            .cornerRadius(10)
+        }
     }
 }
